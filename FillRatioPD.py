0b39e9b5c7164debbc9c3a1c1cdf66d078a33bfb
cesarbartolo <cbartolo@ucdavis.edu>
2017-12-02 17:30:10 -0800

    "Fill ratio calculations file"

---
+++ FillRatioPD.py
@@ -0,0 +1,12 @@
+"""
+Code created to calculate the fill ratio  
+(holes/active) in PDs and its consequences in
+their properties
+
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
