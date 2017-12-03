3b57f4f67c66c29ac4af1711bab61f6980157e3c
cesarbartolo <cbartolo@ucdavis.edu>
2017-12-02 17:43:56 -0800

    Defining areas

--- FillRatioPD.py
+++ FillRatioPD.py
@@ -10,3 +10,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
+
+def area_hole (radio):
+    area_hole= np.pi * radio**2
+    return (area_hole)
+
+def active_area(radio_pd):
+    active_area= np.pi * radio_pd**2
+    return (active_area)
+
+
+
+radio_hole=np.array([ 300,350,500,600,650])*1e-9
+
+area=area_hole(radio_hole)
+print (area)
+plt.plot(radio_hole, area)
+
+
+radio_devices=np.array([15,30,50,125,250])*1e-6    
+act_a=active_area(radio_devices)    
+print (act_a)
